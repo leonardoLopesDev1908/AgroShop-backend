b6b15e1dbcc392d2ef180e9c519a37957322d976
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2025-10-23 21:39:59 -0300

    buscar pedidos rascunho

--- src/main/java/com/dailycodework/agroshop/controller/controllers/PedidoController.java
+++ src/main/java/com/dailycodework/agroshop/controller/controllers/PedidoController.java
@@ -1,17 +1,14 @@
 package com.dailycodework.agroshop.controller.controllers;
 
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dailycodework.agroshop.controller.dto.cadastro.PedidoCadastroDTO;
+import com.dailycodework.agroshop.controller.dto.pesquisa.PedidoPesquisaDTO;
 import com.dailycodework.agroshop.model.Usuario;
 import com.dailycodework.agroshop.response.ApiResponse;
 import com.dailycodework.agroshop.service.Pedido.IPedidoService;
@@ -30,13 +27,15 @@ public class PedidoController {
     @PostMapping("/usuario/pedido")
     public ResponseEntity<ApiResponse> fazerNovoPedido(){
         Usuario usuario = userService.getAuthenticatedUsuario();
-        PedidoCadastroDTO dto = service.fazerPedido(usuario.getId());
+        PedidoPesquisaDTO dto = service.fazerPedido(usuario.getId());
         return ResponseEntity.ok(new ApiResponse("Sucesso", dto));
     }
 
-    @GetMapping("/usuario/{id}/pedidos")
-    public ResponseEntity<ApiResponse> buscarPedido(@PathVariable UUID id){
-        List<PedidoCadastroDTO> lista = service.pedidosUsuario(id);
+    @GetMapping("/usuario/pedidos")
+    public ResponseEntity<ApiResponse> buscarPedido(){
+        Usuario usuario = userService.getAuthenticatedUsuario();
+        List<PedidoPesquisaDTO> lista = service.pedidosUsuario(usuario.getId());
         return ResponseEntity.ok(new ApiResponse("Sucesso", lista));
     }
+  
 }
