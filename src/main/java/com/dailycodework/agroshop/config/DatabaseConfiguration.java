27a5205371c7603f1cc8d7ba89a24cfbe0d49a7f
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2025-12-01 12:26:43 -0300

    mudança docker-compose

--- src/main/java/com/dailycodework/agroshop/config/DatabaseConfiguration.java
+++ src/main/java/com/dailycodework/agroshop/config/DatabaseConfiguration.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -25,7 +26,27 @@ public class DatabaseConfiguration {
     String driver;
 
     @Bean
-    public DataSource hikariDataSource(){
+    @Profile("dev")
+    public DataSource hikariDataSourceDev(){
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+        config.setPoolName("agroshop-db-pool");
+        config.setMaxLifetime(600000);
+        config.setConnectionTimeout(100000);
+        config.setConnectionTestQuery("select 1");
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    @Profile("tests")
+    public DataSource hikariDataSourceTests(){
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(url);
         config.setUsername(username);
