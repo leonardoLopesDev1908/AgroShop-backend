d413fce3fab162d93e4cc7b904b48b8b36b28229
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2025-12-01 22:53:08 -0300

    onda de testes jenkins

--- src/main/java/com/dailycodework/agroshop/controller/controllers/FreteAuthController.java
+++ src/main/java/com/dailycodework/agroshop/controller/controllers/FreteAuthController.java
@@ -13,11 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dailycodework.agroshop.controller.dto.pesquisa.FreteDTO;
 import com.dailycodework.agroshop.response.ApiResponse;
 import com.dailycodework.agroshop.service.Frete.FreteService;
-<<<<<<< HEAD
-=======
 import com.dailycodework.agroshop.service.Produto.ProdutoService;
 import com.dailycodework.agroshop.service.Usuario.UsuarioService;
->>>>>>> ea682802b34cd590ef6ffe4c92700b4f36d15b90
 
 import lombok.RequiredArgsConstructor;
 
@@ -31,12 +28,8 @@ public class FreteAuthController {
 
     @Value("${melhorenvio.token}")
     private String token;
-
-<<<<<<< HEAD
-=======
     private final ProdutoService serviceProduto;
     private final UsuarioService serviceUsuario;
->>>>>>> ea682802b34cd590ef6ffe4c92700b4f36d15b90
     private final FreteService service;
     
     @PostMapping("/frete/produto/cotar")
