9018397b9a6bdae574eba0902f9ad1d3bc2ea3e1
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2025-12-26 16:59:27 -0300

    cors permission for fronted 3.0

--- src/main/java/com/dailycodework/agroshop/config/SecurityConfiguration.java
+++ src/main/java/com/dailycodework/agroshop/config/SecurityConfiguration.java
@@ -93,7 +93,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-            List.of("https://agro-shop-frontend-phi.vercel.app")
+            List.of("http://localhost:5174", "https://agro-shop-frontend-phi.vercel.app")
         );
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
